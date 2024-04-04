dbfc5854ca1583ab8e2ec8a35e2caa3484a07224
Muntajir Hussaingazi <muntajirwork11@gmail.com>
2024-04-04 18:15:53 +0530

    First

---
+++ src/contacts.js
@@ -0,0 +1,26 @@
+const contacts = [
+  {
+    name: "Muntajir",
+    imgURL:
+      "https://freepngimg.com/thumb/anime/26533-9-uchiha-sasuke-transparent-picture-thumb.png",
+    phone: "+91 9587125490",
+    email: "munta@gmail.com"
+  },
+  {
+    name: "Sayantan",
+    imgURL:
+      "https://pngfre.com/wp-content/uploads/anime-poster.png",
+    phone: "+91 8697451288",
+    email: "sayantan@yahoomail.in"
+  },
+  {
+    name: "Snehodipto",
+    imgURL:
+      "https://qph.cf2.quoracdn.net/main-qimg-054fb767362a42f67bfd08690d5e015e-lq",
+    phone: "+91 7845193248",
+    email: "sneha45@gmail.com"
+  }
+];
+
+
+export default contacts;
